f391f0ec2de7fd0a736390a47e79591a22d2065a
Zhangfen21082 <290440677@qq.com>
2020-12-05 15:07:31 +0800

    新增操作

--- C语言数据结构实现/顺序表/SeqList.c
+++ C语言数据结构实现/顺序表/SeqList.c
@@ -16,6 +16,12 @@ void SeqListInit(SeqList* ps)//初始化
 	ps->Size = 0;
 	ps->Capacity = 4;
 }
+void SeqListDestory(SeqList* ps)//销毁
+{
+	free(ps->Array);
+	ps = NULL;
+	ps->Capacity = ps->Size = 0;
+}
 void SeqListPrint(SeqList* ps)//打印
 {
 	assert(ps);
@@ -45,10 +51,13 @@ void SeqListCheckCapacity(SeqList* ps)//检查容量是否满了
 
 void SeqListPushBack(SeqList* ps, SLDataType x)//尾插
 {
+	/*尾插就是size位置上的插入
 	assert(ps);//警告，指针为空
 	SeqListCheckCapacity(ps);//满了就扩容
 	ps->Array[ps->Size] = x;//size表示有效个数，但同时是下一个元素的下标
 	ps->Size++;//元素加1
+	*/
+	SeqListInsert(ps,ps->Size, x);
 }
 void SeqListPopBack(SeqList* ps)//尾删除
 {
@@ -58,6 +67,7 @@ void SeqListPopBack(SeqList* ps)//尾删除
 }
 void SeqListPushFront(SeqList* ps, SLDataType x)//头插
 {
+	/*头插是0位置上的插入
 	assert(ps);
 	SeqListCheckCapacity(ps);//满了就扩容
 	for (int i = ps->Size-1; i >= 0; i--)//头插法：从最后一个元素开始，依次向后移，把0的位置空出来
@@ -66,26 +76,29 @@ void SeqListPushFront(SeqList* ps, SLDataType x)//头插
 	}
 	ps->Array[0] = x;
 	ps->Size++;
+	*/
+	SeqListInsert(ps, 0, x);
 
 }
 void SeqListPopFront(SeqList* ps)//头删
 {
+	/*头删是0位置的删除
 	assert(ps);
 	for (int i = 0; i < ps->Size; i++)
 	{
 		ps->Array[i] = ps->Array[i + 1];//删除时直接覆盖即可
 	}
 	ps->Size--;
+	*/
+	SeqListErase(ps, 0);
+	
 
 }
 void SeqListInsert(SeqList* ps, int pos, SLDataType x)//任意位置插入
 {
 	assert(ps);
+	assert(pos <= ps->Size && pos>= 0);//插入位置有误
 	SeqListCheckCapacity(ps);
-	if (pos > ps->Size || pos < 0)//插入位置有误
-	{
-		exit(-1);
-	}
 	for (int i = ps->Size - 1; i >= pos; i--)//从插入位置以后依次向后挪动
 	{
 		ps->Array[i + 1] = ps->Array[i];
@@ -97,11 +110,27 @@ void SeqListInsert(SeqList* ps, int pos, SLDataType x)//任意位置插入
 void SeqListErase(SeqList* ps, int pos)//任意位置删除
 {
 	assert(ps);
+	assert(pos < ps->Size && pos >= 0);//删除位置有误
 	for (int i = pos; i < ps->Size; i++)
 	{
 		ps->Array[i] = ps->Array[i + 1];
 	}
 	ps->Size--;
+}
+
+int  SeqListFind(SeqList* ps, SLDataType x)//顺序查找法
+{
+	assert(ps);
+	int i = 0;
+	while (i<ps->Size)
+	{
+		if (ps->Array[i] == x)
+			return i;
+		++i;
+	}
+	return -1;
+
+
 }
 int  SeqListFindvalue_Bind(SeqList* ps, int pos)//二分查找法
 {
--- C语言数据结构实现/顺序表/SeqList_test.c
+++ C语言数据结构实现/顺序表/SeqList_test.c
@@ -1,14 +1,15 @@
 #include "SeqList.h"
 
-//测试头尾插入删除
-void Test1()
+//测试插入删除元素
+void Test1(SeqList* ps)
 {
-	SeqList s;
-	SeqListInit(&s);//初始化
-	SeqListPushBack(&s, 1);
-	SeqListPushBack(&s, 2);
-	SeqListPushBack(&s, 3);//尾部插入依次元素
-	SeqListPrint(&s);
+	SeqListInit(ps);//初始化
+	SeqListPushBack(ps, 1);
+	SeqListPushBack(ps, 2);
+	SeqListPushBack(ps, 3);
+	SeqListPushBack(ps, 4);
+	SeqListPushBack(ps, 5);//尾部插入依次元素
+	SeqListPrint(ps);
 
 	//SeqListPopBack(&s);
 	//SeqListPopBack(&s);
@@ -17,7 +18,9 @@ void Test1()
 
 	//SeqListPushFront(&s, 9);
 	//SeqListPushFront(&s, 8);
-	//SeqListPushFront(&s, 7);//头部依次插入元素
+	//SeqListPushFront(&s, 7);
+	//SeqListPushFront(&s, 6);
+	//SeqListPushFront(&s, 5);//头部依次插入元素
 	//SeqListPrint(&s);
 
 	//SeqListPopFront(&s);
@@ -25,21 +28,40 @@ void Test1()
 	//SeqListPopFront(&s);//头部依次删除元素
 	//SeqListPrint(&s);
 
-	SeqListInsert(&s, 1, 5);
-	SeqListInsert(&s, 1, 6);//向指定位置插入元素
-	SeqListPrint(&s);
+	//SeqListInsert(&s, 1, 5);
+	//SeqListInsert(&s, 1, 6);//向指定位置插入元素
+	//SeqListPrint(&s);
 
-	SeqListErase(&s, 1);
-	SeqListErase(&s, 1);//把指定位置元素删除
-	SeqListPrint(&s);
+	//SeqListErase(&s, 1);
+	//SeqListErase(&s, 1);//把指定位置元素删除
+	//SeqListPrint(&s);
 
-	printf("这个元素是存在的它的下表是%d\n", SeqListFindvalue_Bind(&s, 0));//使用二分查找某个元素并输出
+}
+//测试接受用户输入删除指定元素删除
+void Test2(SeqList* ps)
+{
+	printf("请输入要删除的元素\n");
+	int res = 0;
+	scanf_s("%d", &res);
+	int index = SeqListFind(ps, res);
+	if (index == -1)
+		printf("没有此元素");
+	else
+	{
+		printf("删除后为\n");
+		SeqListErase(ps, index);
+		SeqListPrint(ps);
+	}
+	
+	
 
 }
 
 
 int main()
 {
-	Test1();
+	SeqList s;
+	Test1(&s);//测试插入删除元素
+	Test2(&s);//测试用户指定元素删除
 	return 0;
 }
